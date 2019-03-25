a898f86a7bb2b19d5f792719b291d47bfe6b1968
linlurui <rocky233@126.com>
2019-03-25 16:20:28 +0800

    Update CcwebAppliction.java

--- ccweb-core/src/main/java/ccait/ccweb/CcwebAppliction.java
+++ ccweb-core/src/main/java/ccait/ccweb/CcwebAppliction.java
@@ -6,11 +6,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
+
+@EnableHystrixDashboard
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableEurekaClient
+@EnableHystrix
+@EnableZuulProxy
 @EnableWebFlux
-@SpringBootApplication( exclude = {DataSourceAutoConfiguration.class,
-        DataSourceTransactionManagerAutoConfiguration.class,} )
+@SpringBootApplication( exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class,} )
 public class CcwebAppliction {
 
     private static final Logger log = LogManager.getLogger( CcwebAppliction.class );
@@ -29,6 +41,8 @@ public class CcwebAppliction {
             app.run(clazz, args);
         }
 
+        log.info( "ccweb success start!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" );
+        log.info( "ccweb success start!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" );
         log.info( "ccweb success start!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" );
     }
 }
