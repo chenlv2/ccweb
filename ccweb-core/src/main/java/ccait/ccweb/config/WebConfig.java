91a355442d3666bf06a4f258924ed3b08ed2ac18
linlurui <rocky233@126.com>
2019-03-25 19:18:30 +0800

    Update WebConfig.java

--- ccweb-core/src/main/java/ccait/ccweb/config/WebConfig.java
+++ ccweb-core/src/main/java/ccait/ccweb/config/WebConfig.java
@@ -13,7 +13,7 @@ package ccait.ccweb.config;
 
 import ccait.ccweb.context.EntityContext;
 import ccait.ccweb.context.TriggerContext;
-import ccait.ccweb.filter.SecurityFilter;
+import ccait.ccweb.filter.UrlFilter;
 import ccait.ccweb.interceptor.SecurityInterceptor;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
@@ -74,7 +74,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public FilterRegistrationBean requestFilter()
     {
-        FilterRegistrationBean bean = new FilterRegistrationBean(new SecurityFilter());
+        FilterRegistrationBean bean = new FilterRegistrationBean(new UrlFilter());
         bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
 
         return bean;
