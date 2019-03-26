d552f004947a7ab792586f55686b9f90d496b7e8
linlurui <rockylin@qq.com>
2019-03-26 11:18:23 +0800

    Create StaticVars.java

---
+++ ccweb-core/src/main/java/ccait/ccweb/utils/StaticVars.java
@@ -0,0 +1,11 @@
+package ccait.ccweb.utils;
+
+public final class StaticVars {
+    public static final String VARS_PATH = "org.springframework.web.servlet.HandlerMapping.uriTemplateVariables";
+    public static final String CURRENT_DATASOURCE = "_CURRENT_DATASOURCE_";
+    public final static String LOG_PRE_SUFFIX = "【ccweb】";
+    public static final String LOGIN_KEY = "_USER_LOGIN_";
+    public static final String CURRENT_TABLE = "_CURRENT_TABLE_";
+    public static final String CURRENT_MAX_PRIVILEGE_SCOPE = "_CURRENT_MAX_PRIVILEGE_SCOPE_";
+    public static final String NO_PRIVILEGE_MESSAGE = "没有足够的权限执行该操作(No Privilege)";
+}
