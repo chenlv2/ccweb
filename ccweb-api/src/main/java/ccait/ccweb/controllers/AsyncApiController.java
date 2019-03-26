91b5f8bc09400447de5e71899e4bdf3886210469
linlurui <rockylin@qq.com>
2019-03-26 11:27:52 +0800

    Update AsyncApiController.java

--- ccweb-api/src/main/java/ccait/ccweb/controllers/AsyncApiController.java
+++ ccweb-api/src/main/java/ccait/ccweb/controllers/AsyncApiController.java
@@ -22,7 +22,7 @@ import reactor.core.publisher.Mono;
 import java.util.List;
 import java.util.Map;
 
-import static ccait.ccweb.context.ApplicationContext.LOG_PRE_SUFFIX;
+import static ccait.ccweb.utils.StaticVars.LOG_PRE_SUFFIX;
 
 
 @RestController
