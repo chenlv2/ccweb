5e0c36db894b85c935e755fe23611e52cc045dd0
linlurui <rockylin@qq.com>
2019-03-27 18:12:48 +0800

    Update TableInfo.java

--- ccweb-core/src/main/java/ccait/ccweb/model/TableInfo.java
+++ ccweb-core/src/main/java/ccait/ccweb/model/TableInfo.java
@@ -1,5 +1,7 @@
 package ccait.ccweb.model;
 
+import ccait.ccweb.enums.PrivilegeScope;
+
 import java.lang.reflect.Field;
 import java.util.List;
 
@@ -36,8 +38,17 @@ public class TableInfo {
         this.entity = entity;
     }
 
+    public PrivilegeScope getPrivilegeScope() {
+        return privilegeScope;
+    }
+
+    public void setPrivilegeScope(PrivilegeScope privilegeScope) {
+        this.privilegeScope = privilegeScope;
+    }
+
     private String tablename;
     private String alias;
     private List<Field> fields;
     private Object entity;
+    private PrivilegeScope privilegeScope;
 }
